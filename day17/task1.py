086261f874aec7d9c8bfb98c4fa46ca5c4cb992c
NKaty <nd.katy@gmail.com>
2019-12-18 23:48:52 +0300

    add solution to the day17 tasks

--- day17/task1.py
+++ day17/task1.py
@@ -12,7 +12,8 @@ def count_alignment_params_sum(data, tiles):
     shifts = ((0, 1), (0, -1), (1, 0), (-1, 0))
     for row in range(1, len(grid) - 2):
         for col in range(1, len(grid[0]) - 1):
-            if grid[row][col] == 35 and all([grid[row + shift[0]][col + shift[1]] == 35 for shift in shifts]):
+            if grid[row][col] == tiles['scaffold'] and all(
+                    [grid[row + shift[0]][col + shift[1]] == tiles['scaffold'] for shift in shifts]):
                 alignment_params_sum += row * col
     return alignment_params_sum
 
@@ -21,5 +22,4 @@ if __name__ == "__main__":
     with open('day17/input.txt') as inp:
         ns = list(map(int, inp.read().strip().split(',')))
 
-    # print(get_scaffold_grid(ns, {35: '#', 46: '.', 10: '\n'}))
-    print(count_alignment_params_sum(ns, {}))
+    print(count_alignment_params_sum(ns, {'scaffold': 35, 'open_space': 46}))  # 4112
