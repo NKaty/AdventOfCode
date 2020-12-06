4d3a2a7103fe5fe5a023bb6df605e4ff929b8952
NKaty <nd.katy@gmail.com>
2020-12-06 13:06:05 +0300

    add solutions to the day6 tasks

---
+++ 2020/day06/task1.py
@@ -0,0 +1,9 @@
+def find_count_sum(answers):
+    return sum(len(set.union(*answer)) for answer in answers)
+
+
+if __name__ == "__main__":
+    with open('day06/input.txt') as inp:
+        data = ((set(ans) for ans in item.strip().split()) for item in inp.read().split('\n\n'))
+
+    print(find_count_sum(data))  # 6799
